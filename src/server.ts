b17c6e21e7802e51790961ff3172137a55cedbe3
Washington <was.antonio.xy@gmail.com>
2026-01-03 17:22:09 -0400

    adding zod to validate env varibles

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import { randomUUID } from 'crypto'
 import { knex } from './database';
+import { env } from './env';
 
 const app = fastify()
 
@@ -20,7 +21,7 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP server running');
