e9e04b3de0dbbe2ce95371abcfb4f27d9583e2cf
Washington <was.antonio.xy@gmail.com>
2026-01-04 17:28:47 -0400

    Summary transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -21,6 +21,11 @@ export async function transactionsRoutes(app: FastifyInstance) {
         return { transaction }
     })
 
+    app.get('/summary', async () => {
+        const summary = await knex('transactions').sum('amount', { as: 'amount'}).first()
+        return { summary }
+    })
+
     app.post('/', async (request, reply) => {
         // { title, amount, type: credit | debit }
 
